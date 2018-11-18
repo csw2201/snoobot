51c178b98656724ea7b91d186b9fca5633c8097b
csw2201 <45121140+csw2201@users.noreply.github.com>
2018-11-18 15:23:17 +0900

    Add files via upload

---
+++ kakatalk_lunch_bot.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+
+
+from resources.keyboard import Keyboard
+from resources.message import Message
+
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+api = Api(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+api.add_resource(Keyboard, '/keyboard')
+api.add_resource(Message, '/message')
+
+if __name__ == '__main__':
+    app.run()
